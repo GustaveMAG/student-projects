3e33d8c504430faf8736e0c6e96702929aa62039
Gustave <gustave2mag@gmail.com>
2026-05-13 22:16:01 +0200

    fix: point frontend API to Railway backend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : '/api',
+});
 
 // Attache le JWT à chaque requête
 api.interceptors.request.use((config) => {
